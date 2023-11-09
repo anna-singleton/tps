4542f69a533586428d2d4a68873347e5259f4590
anna-singleton <annabeths111@gmail.com>
2023-11-09 22:12:36 +0000

    working draft of MVP

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,145 @@ use toml;
 use serde::Deserialize;
 use git2::Repository;
 use std::collections::HashSet;
-use tmux_interface::{tmux::Tmux, list_sessions::ListSessions, SwitchClient};
+use tmux_interface::{tmux::Tmux, list_sessions::ListSessions, SwitchClient, NewSession, AttachSession};
+use skim::prelude::*;
+use regex::{Regex, RegexBuilder};
 
 #[derive(Deserialize, Debug)]
 struct Config {
     project_homes: Vec<String>,
 }
 
-fn main() {
-    let projects = generate_project_dirs();
-    println!("{:#?}", projects);
-
-    let sessions = get_tmux_session_names();
-    println!("{:?}", sessions);
-
-    // if let Err(e) = Tmux::with_command(SwitchClient::new().target_session("test1").build()).output() {
-    //     eprintln!("could not switch client with error {}", e);
-    // }
+#[derive(Debug, Clone)]
+struct Session {
+    name: String,
+    window_count: u32,
+    date_created: String,
+    attached: bool,
 }
 
-fn get_tmux_session_names() -> Vec<String> {
+#[derive(Debug)]
+struct Project {
+    path: PathBuf,
+    path_name: String,
+    session: Option<Session>,
+}
+
+impl Project {
+    fn new(path: PathBuf, sessions: &Vec<Session>) -> Project {
+        let path_name = path.display().to_string();
+        for (i, s) in sessions.iter().enumerate() {
+            if path_name == s.name {
+                let s = sessions[i].clone();
+                return Project { path, path_name, session: Some(s) }
+            }
+        }
+
+        // no session recorded.
+        return Project { path, path_name, session: None };
+    }
+}
+
+impl SkimItem for Project {
+    fn text(&self) -> Cow<str> {
+        return Cow::Borrowed(&self.path_name);
+    }
+
+    fn preview(&self, _context: PreviewContext) -> ItemPreview {
+        let s = match &self.session {
+            Some(session) => format!("Test preview for {}, which has session name {}",
+                        self.path_name, session.name),
+            None => format!("Test preview for {}, which has no existing session",
+                        self.path_name),
+        };
+        return ItemPreview::Text(s.to_owned());
+    }
+}
+
+fn main() {
+    let project_paths = generate_project_dirs();
+    println!("{:#?}", project_paths);
+
+    let sessions = get_tmux_session_info();
+    println!("{:?}", sessions);
+
+    let projects:Vec<_> = project_paths.into_iter().map(|path| Project::new(path, &sessions)).collect();
+
+    let skim_opts = SkimOptionsBuilder::default()
+        .multi(false)
+        .build()
+        .unwrap();
+
+    let (tx, rx): (SkimItemSender, SkimItemReceiver) = unbounded();
+
+    for p in projects.into_iter() {
+        tx.send(Arc::new(p)).expect("failed to send project {:?} to skim");
+    }
+
+    drop(tx);
+
+    let selected_skim_items = Skim::run_with(&skim_opts, Some(rx))
+        .map(|out| out.selected_items)
+        .unwrap_or_else(Vec::new);
+
+    let selected_proj = selected_skim_items.iter()
+        .map(|selected| (**selected).as_any().downcast_ref::<Project>().unwrap().to_owned())
+        .collect::<Vec<_>>()[0];
+
+
+    if selected_proj.session.is_none() {
+        println!("Session does not exist yet! making one.");
+        let new_session = match Tmux::with_command(NewSession::new()
+                           .start_directory(&selected_proj.path_name)
+                           .session_name(&selected_proj.path_name)
+                           .detached()
+                           .build()).output() {
+                            Ok(s) => s,
+                            Err(e) => panic!("failed running tmux command with error {}", e),
+                        };
+        dbg!(&new_session);
+    }
+
+    println!("Attempting to switch to project: {}", selected_proj.path_name);
+
+    if let Err(e) = std::env::var("TMUX") {
+        match e {
+            std::env::VarError::NotPresent => Tmux::with_command(AttachSession::new().target_session(&selected_proj.path_name).build()).output().expect("failed to attach non-attached client to session"), // we are not in tmux right now
+            std::env::VarError::NotUnicode(_) => panic!("$TMUX is not unicode! this cannot be handled. exiting."),
+        };
+    } else {
+        if let Err(e) = Tmux::with_command(SwitchClient::new().target_session(&selected_proj.path_name).build()).output() {
+            eprintln!("could not switch client with error {}", e);
+        }
+    }
+
+}
+
+fn get_tmux_session_info() -> Vec<Session> {
     let cmd_output = Tmux::with_command(ListSessions::new().build()).output().expect("could not run tmux command").0.stdout;
     let s = std::str::from_utf8(&cmd_output).expect("could not convert output from utf8.");
-    let sessions = s.lines()
-        .map(|x| x.split_once(":").expect("bad tmux output detected").0.to_string())
-        .collect();
 
+    let re_data = RegexBuilder::new(r"(\S*?): (\d+) windows \(created (.*?)\)")
+        .multi_line(true)
+        .build()
+        .unwrap();
+    let re_attached = Regex::new(r"\(attached\)").unwrap();
+
+    let mut sessions = Vec::new();
+    for line in s.lines() {
+        let hits = re_data.captures(line).unwrap();
+        let name = hits.get(1).unwrap().as_str().to_string();
+        let window_count: u32 = hits.get(2).unwrap().as_str().parse().unwrap();
+        let date_created = hits.get(3).unwrap().as_str().to_string();
+        let attached = re_attached.is_match(line);
+
+        sessions.push(Session {
+            name,
+            window_count,
+            date_created,
+            attached,
+        });
+    }
     return sessions;
 }
 
